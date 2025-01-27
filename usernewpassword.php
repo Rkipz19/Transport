a023d4d6a1f4e97c1e1ea0c1ec24bb8444c1fea0
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-01-27 21:23:22 +0300

    Modified the file by changing to resetpassword_process()

--- usernewpassword.php
+++ usernewpassword.php
@@ -1,10 +1,7 @@
 <?php
 require_once 'AutoLoad.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1); 
-error_reporting(E_ALL);
 
 $Objlayout -> header();
-$ObjProcesses -> newpassword();
-$ObjUserForm -> newpassword_form();
+$ObjProcesses -> resetpassword_process();
+$ObjUserForm -> reset_password();
 $Objlayout -> footer();
