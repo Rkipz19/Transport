31e15655dc5632b8c339dc4102276add8c5ec8e6
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-02-17 20:19:11 +0300

    Added css links

--- classes/layout.php
+++ classes/layout.php
@@ -11,7 +11,10 @@ class layout{
 <title>Urban Links Transport</title>
 <link rel = "icon" type = "image/x-icon" href = "images/favicon.ico" >
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <link rel = "stylesheet" href = "css/style.css">
+<link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/jumbotron/">
+<link rel = "stylesheet" href = "css/bootstrap.min.css">
 </head>
 <body>
             <?php
@@ -19,8 +22,9 @@ class layout{
     public function footer(){
         ?>
        <!--Bootstrap bundle with popper-->
-<script src = "js/script.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src = "js/script.js"></script>
+<script src = "js/main.js"></script>
 </body>
 </html>
         <?php
