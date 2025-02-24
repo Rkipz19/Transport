db2380dbf875deeb1700c065b6aa5f2a9ff60a5f
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-02-24 12:51:32 +0300

    Modified layout added datatables link

--- classes/layout.php
+++ classes/layout.php
@@ -15,6 +15,7 @@ class layout{
 <link rel = "icon" type = "image/x-icon" href = "images/favicon.ico" >
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" />
 <link rel = "stylesheet" href = "css/style.css">
 <link rel = "stylesheet" href = "css/bootstrap.min.css">
 </head>
@@ -26,8 +27,17 @@ class layout{
     public function footer(){
         ?>
        <!--Bootstrap bundle with popper-->
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script>
+  $(document).ready( function () {
+    $('#myTable').DataTable();
+  });
+</script>
+<script src="js/chart.js"></script>
 <script src = "js/script.js"></script>
 <script src = "js/main.js"></script>
 </body>
