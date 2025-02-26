af6596bbef63efbae03774da41032ca22b4abb19
Rkipz19 <ronnie.kipkoech@strathmore.edu>
2025-02-26 05:28:44 +0300

    Modified the page container

--- driverreg.php
+++ driverreg.php
@@ -2,5 +2,6 @@
 require_once "AutoLoad.php";
 $Objlayout -> header();
 $ObjAdminProcesses -> driverRegistration();
+$Objadminpagelayout -> sidebar();
 $ObjAdminForm ->registerDriver();
 $Objlayout -> footer();
